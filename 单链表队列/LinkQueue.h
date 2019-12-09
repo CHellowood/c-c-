84877fb85d0ae02dd9855aeeb4a0a6eb7af43488
CHellowood <2045005059@qq.com>
2019-12-09 11:50:21 +0800

    2019_12_9

---
+++ 单链表队列/LinkQueue.h
@@ -0,0 +1,30 @@
+//#pragma once
+#ifndef _LINKQUEUE_H_
+#define _LINKQUEUE_H_
+
+#define QUEUE_MAXLEN 5
+
+typedef int DataType;
+
+typedef struct Link {
+	DataType data;
+	Link* naxt;
+}LINK;
+
+typedef LINK* LINK_P;
+
+typedef struct Queue {
+	int length;
+	LINK_P head;
+	LINK_P tail;
+}QUEUE;
+
+bool initQueue(QUEUE* queue);                 //初始化队列
+//bool emptyQueue(QUEUE* queue);                //检查Queue是否为空
+//bool fullQueue(QUEUE* queue);                 //检查Queue是否为满
+bool addNode(QUEUE* queue, DataType* data);   //入队
+bool deleteNode(QUEUE* queue, DataType* data);//出队
+bool clearQueue(QUEUE* queue);                //清空队列
+void printQueue(QUEUE* queue);                //打印队列
+
+#endif 
